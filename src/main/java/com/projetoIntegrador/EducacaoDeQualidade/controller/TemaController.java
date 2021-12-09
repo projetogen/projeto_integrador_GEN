79b8dde3296e5540a82276169d31961e41811834
Rafael Borges <rafaelborges.av@gmail.com>
2021-12-09 10:45:25 -0300

    @okborges [task: criando controller]

---
+++ src/main/java/com/projetoIntegrador/EducacaoDeQualidade/controller/TemaController.java
@@ -0,0 +1,39 @@
+package com.projetoIntegrador.EducacaoDeQualidade.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.projetoIntegrador.EducacaoDeQualidade.model.TemaModel;
+import com.projetoIntegrador.EducacaoDeQualidade.repository.TemaRepository;
+
+@RestController
+@RequestMapping("/tema")
+@CrossOrigin("*")
+
+public class TemaController {
+	@Autowired
+	private TemaRepository repository;
+
+	@GetMapping
+	public ResponseEntity<List<TemaModel>> GetAll() {
+		return ResponseEntity.ok(repository.findAll());
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<TemaModel> GetById(@PathVariable Long id) {
+		return repository.findById(id).map(resp -> ResponseEntity.status(200).body(resp))
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Id não existente"));
+	}
+	
+	
+
+}
---
+++ src/main/java/com/projetoIntegrador/EducacaoDeQualidade/repository/TemaRepository.java
@@ -0,0 +1,15 @@
+package com.projetoIntegrador.EducacaoDeQualidade.repository;
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projetoIntegrador.EducacaoDeQualidade.model.TemaModel;
+
+
+public interface TemaRepository extends JpaRepository<TemaModel, Long> {
+	public List<TemaModel> FindAllByAssunto(String assunto);
+}
+
+
