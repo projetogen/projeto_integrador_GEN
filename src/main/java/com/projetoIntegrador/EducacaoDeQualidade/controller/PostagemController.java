d1aac953b92b88e3ea8d23a4e5b72c98ea1ba682
mariana-nogueira21 <mariana.nogueira21@gmail.com>
2021-12-10 13:30:19 -0300

    @mariana-nogueira21 [Task: Criação dos método put e delete na PostagemController]

--- src/main/java/com/projetoIntegrador/EducacaoDeQualidade/controller/PostagemController.java
+++ src/main/java/com/projetoIntegrador/EducacaoDeQualidade/controller/PostagemController.java
@@ -8,9 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,6 +48,16 @@ public class PostagemController {
 	@PostMapping("/save")
 	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem) {
 		return ResponseEntity.status(201).body(repository.save(postagem));
+	}
+	
+	@PutMapping("/update")
+	public ResponseEntity<Postagem> put (@Valid @RequestBody Postagem postagem){
+		return ResponseEntity.status(200).body(repository.save(postagem));
+	}
+	
+	@DeleteMapping("/remove/{id}")
+	public void delete(@PathVariable Long id) {
+		repository.deleteById(id);
 	}
 
 }
