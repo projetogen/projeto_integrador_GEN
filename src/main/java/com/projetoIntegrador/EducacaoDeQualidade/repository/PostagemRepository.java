a06898c84a1a3a8f7ff0b8783f5fc39a388f1936
Vivian-Santana <santanavivian245@gmail.com>
2021-12-10 12:17:35 -0300

    @Vivian-Santana[task: PostagemRepository]

--- src/main/java/com/projetoIntegrador/EducacaoDeQualidade/repository/PostagemRepository.java
+++ src/main/java/com/projetoIntegrador/EducacaoDeQualidade/repository/PostagemRepository.java
@@ -1,17 +1,14 @@
 package com.projetoIntegrador.EducacaoDeQualidade.repository;
 
-
 import java.util.List;
-
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.projetoIntegrador.EducacaoDeQualidade.model.TemaModel;
+import com.projetoIntegrador.EducacaoDeQualidade.model.Postagem;
 
 @Repository
-public interface TemaRepository extends JpaRepository<TemaModel, Long> {
-	public List<TemaModel> findAllByAssunto(String assunto);
+public interface PostagemRepository extends JpaRepository<Postagem, Long>{
+ 
+	public List<Postagem> findByTituloContainingIgnoreCase(String Titulo);
 }
-
-
