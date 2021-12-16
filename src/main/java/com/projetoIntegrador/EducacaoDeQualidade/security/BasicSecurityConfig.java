94dfd470cb428b5fbe8d2f70c4d33a8aa2fb44b9
Murilo Feitosa <muriloalf@gmail.com>
2021-12-16 16:17:13 -0300

    @MuriloAugusto1 [task: criação de UsuarioService e UserManagementController ]

--- src/main/java/com/projetoIntegrador/EducacaoDeQualidade/security/BasicSecurityConfig.java
+++ src/main/java/com/projetoIntegrador/EducacaoDeQualidade/security/BasicSecurityConfig.java
@@ -31,8 +31,8 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-		.antMatchers("/usuarios/logar").permitAll()
-		.antMatchers("/usuarios/cadastrar").permitAll()
+		.antMatchers("/users/logar").permitAll()
+		.antMatchers("/users/cadastrar").permitAll()
 		.anyRequest().authenticated()
 		.and().httpBasic()
 		.and().sessionManagement()
